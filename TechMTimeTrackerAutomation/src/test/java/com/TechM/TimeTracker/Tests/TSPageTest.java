b6dd74eb2aef17102d141e650d9e1a783f2e4da4
Praveen Koppula <koppulapraveen@gmail.com>
2019-04-05 14:00:10 -0700

    - Added a test in TSPage to see the current week & its range

---
+++ TechMTimeTrackerAutomation/src/test/java/com/TechM/TimeTracker/Tests/TSPageTest.java
@@ -0,0 +1,43 @@
+package com.TechM.TimeTracker.Tests;
+
+import com.techm.timetracker.base.TestBase;
+import com.techm.timetracker.pages.HomePage;
+import com.techm.timetracker.pages.LoginPage;
+import com.techm.timetracker.pages.TimeSheetPage;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TSPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	TimeSheetPage tsPage;
+
+	public TSPageTest()
+	{
+		super();
+	}
+	
+	@BeforeTest
+	public void setup()
+	{
+		initialization();
+		loginPage = new LoginPage();
+		homePage =  loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		tsPage = homePage.enterToTSPage();
+	}
+
+	@Test (priority=1)
+	public void selectPresentWeek()
+	{
+		int weeksPassed = tsPage.ListOfWeeks().size();
+		String presentWeekRange = tsPage.ListOfWeeks().get(weeksPassed-1).getText();
+		System.out.println("Present week range: " + presentWeekRange);
+	}
+	
+	@AfterTest
+	public void teardown()
+	{
+		driver.quit();
+	}
+}
