b80644e17edf2e16a10f7c38e0cb5670668cec90
Praveen Koppula <koppulapraveen@gmail.com>
2019-04-08 00:13:36 -0700

    - Changed conditions to exit if current day TS is filled

--- TechMTimeTrackerAutomation/src/test/java/com/TechM/TimeTracker/Tests/TSPageTest.java
+++ TechMTimeTrackerAutomation/src/test/java/com/TechM/TimeTracker/Tests/TSPageTest.java
@@ -8,6 +8,13 @@ import com.techm.timetracker.utils.TestUtils;
 
 import static org.testng.Assert.assertEquals;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -31,7 +38,33 @@ public class TSPageTest extends TestBase{
 		tsPage = homePage.enterToTSPage();
 	}
 
-	@Test (priority=1, enabled = false)
+	@Test (priority=1, enabled = true)
+	public void verifyCurrentWeekIsdisplayed() throws InterruptedException
+	{
+		boolean bCurrentWeekIsdisplayed = false;
+		//To get the text of last option from the week range combo
+		int currentWeekNumber = tsPage.listOfWeeks().size()-1;
+		String presentWeekRange = tsPage.listOfWeeks().get(currentWeekNumber).getText();
+		System.out.println("Present week range: " + presentWeekRange);		
+		//Trim week number from the complete text 
+		String weekNumberFromUI= presentWeekRange.substring(9, 11);
+		System.out.println("Week number from site: " + weekNumberFromUI);
+		
+		Calendar cal = Calendar.getInstance();		
+		//Get WEEK_OF_YEAR from Calendar class
+		System.out.println("Week of the year from calendar obj : " + cal.get(Calendar.WEEK_OF_YEAR));
+		
+		if(Integer.parseInt(weekNumberFromUI) == cal.get(Calendar.WEEK_OF_YEAR)){
+			System.out.println("Week number matched. Current week displayed !!!");
+			bCurrentWeekIsdisplayed = true;
+		}else {
+			System.out.println("Week number NOT matched. Current week NOT displayed !!!");
+		}
+
+		assertEquals(bCurrentWeekIsdisplayed, true, "Week number NOT matched");		
+	}
+	
+	@Test (priority=2, enabled = true)
 	public void checkAndFillPresentWeekTest() throws InterruptedException
 	{
 		int currentWeekNumber = tsPage.listOfWeeks().size()-1;
@@ -47,15 +80,24 @@ public class TSPageTest extends TestBase{
 			System.out.println("Timesheets not filled/partially filled for current week");
 			for(int dayNumber=0; dayNumber <5; dayNumber++)
 			{
-				if(!tsPage.bDayTimesheetFilled(dayNumber))
+				if(!tsPage.isItCurrentDay(dayNumber))
 				{
-					tsPage.enterTimeForADay(dayNumber);
+					if(!tsPage.bDayTimesheetFilled(dayNumber))
+						tsPage.enterTimeForADay(dayNumber);
+				}
+				else 
+				{
+					if(!tsPage.bDayTimesheetFilled(dayNumber) )
+					{
+						tsPage.enterTimeForADay(dayNumber);
+						break;
+					}
 				}
 			}			
 		}
 	}
-
-	@Test (priority=2, enabled = true)
+	
+	@Test (priority=3, enabled = true)
 	public void checkAndFillTSFromASpecificWeekTest() throws InterruptedException
 	{
 		int currentWeekNumber = tsPage.listOfWeeks().size()-1;
@@ -72,20 +114,29 @@ public class TSPageTest extends TestBase{
 			}
 			else
 			{
-				System.out.println("Timesheets not filled/partially filled for week" + TestUtils.REQUIRED_WEEK_NUMBER);
+				System.out.println("Timesheets not filled/partially filled for week" + i);
 				for(int dayNumber=0; dayNumber <5; dayNumber++)
 				{
-					if(!tsPage.bDayTimesheetFilled(dayNumber))
+					if(!tsPage.isItCurrentDay(dayNumber))
 					{
-						tsPage.enterTimeForADay(dayNumber);
+						if(!tsPage.bDayTimesheetFilled(dayNumber))
+							tsPage.enterTimeForADay(dayNumber);
 					}
-				}			
+					else 
+					{
+						if(!tsPage.bDayTimesheetFilled(dayNumber) )
+						{
+							tsPage.enterTimeForADay(dayNumber);
+							break;
+						}
+					}
+				}
 			}
 		}
 	}
 
 
-	@Test (priority=3, enabled = true)
+	@Test (priority=4, enabled = true)
 	public void signOutTest() throws InterruptedException
 	{
 		tsPage.signOut();			
