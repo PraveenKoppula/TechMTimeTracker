102d9fe40924038ab24d20569fb618983d69c006
Praveen Koppula <koppulapraveen@gmail.com>
2019-04-08 20:56:19 -0700

    - Capture screenshot method added

--- TechMTimeTrackerAutomation/src/main/java/com/techm/timetracker/utils/TestUtils.java
+++ TechMTimeTrackerAutomation/src/main/java/com/techm/timetracker/utils/TestUtils.java
@@ -1,5 +1,12 @@
 package com.techm.timetracker.utils;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import com.techm.timetracker.base.TestBase;
 
 public class TestUtils extends TestBase{
@@ -8,4 +15,10 @@ public class TestUtils extends TestBase{
 	public static long IMPLICIT_WAIT = 20;
 	public static int REQUIRED_WEEK_NUMBER = 10;
 	public static String LOGIN_URL = "https://timetracker.techmahindra.com/mytime/Login.aspx"; 
+	
+	public static void takeScreenshotAtEndOfTest() throws IOException {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+	}
 }
