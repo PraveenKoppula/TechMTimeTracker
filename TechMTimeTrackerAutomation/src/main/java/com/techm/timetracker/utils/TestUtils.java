d7dbd5810cb983b6e4cbee43887e7896593abeb9
Praveen Koppula <koppulapraveen@gmail.com>
2019-07-15 14:25:40 -0700

    - "Test" is added in test method names.(Naming convention)

--- TechMTimeTrackerAutomation/src/main/java/com/techm/timetracker/utils/TestUtils.java
+++ TechMTimeTrackerAutomation/src/main/java/com/techm/timetracker/utils/TestUtils.java
@@ -13,9 +13,13 @@ public class TestUtils extends TestBase{
 
 	public static long PAGE_LOAD_TIMEOUT = 30;
 	public static long IMPLICIT_WAIT = 20;
-	public static int REQUIRED_WEEK_NUMBER = 10;
-	public static String LOGIN_URL = "https://timetracker.techmahindra.com/mytime/Login.aspx"; 
-	
+	public static int REQUIRED_WEEK_NUMBER = 27;
+	public static String LOGIN_URL = "https://timetracker.techmahindra.com/mytime/Login.aspx";
+	public static int i_workSELECTION =1;
+	public static int i_projectSelection =1;
+	public static int i_taskSelection =1;
+
+
 	public static void takeScreenshotAtEndOfTest() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
